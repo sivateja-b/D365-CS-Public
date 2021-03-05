9838aafed581241b57e9cc3c952421dda8a886a8
Siva Teja Behara <63949792+sivateja-b@users.noreply.github.com>
2021-03-05 19:53:40 +0545

    Deserialization Extension class
    Extension class sample required for response deserialization.

---
+++ APIHelperExtension_Sample.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using STB.RESTFulIntegration;
+
+namespace IntegrationExtensionSample
+{
+    public class APIHelperExtension_Sample : APIHelper
+    {
+        public override object deserializeJSON(string _JSON, Type _responseObjectType)
+        {
+            object ret = null;
+            switch (_responseObjectType.Name)
+            {
+                case nameof(ResponseModelClass):
+                    ret = this.ResponseDeserializeInternal<ResponseModelClass>(_JSON);
+                    break;
+                default:
+                    throw new Exception("Switch doesn't handle the case for: " + _responseObjectType.Name + ". Method name:" + MethodBase.GetCurrentMethod().Name);
+            }
+            return ret;
+        }
+    }
+}

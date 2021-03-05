ee871cf4a30632e9dc3bc40ae7c7f649d4f4147f
Siva Teja Behara <63949792+sivateja-b@users.noreply.github.com>
2021-03-05 20:51:29 +0545

    specific helper

---
+++ SpecificHelperExtension_sample.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using STB.RESTFulIntegration;
+
+namespace IntegrationExtension
+{
+    class SpecificHelperExtension : SpecificHelper
+    {
+        public override IList addObjectToList(object _object, IList List = null)
+        {
+            switch(_object.GetType().Name)
+            {
+                case nameof(System.String):
+                    System.String str = _object as System.String;
+                return this.addObjectToListInternal<System.String>(str, List);
+                case nameof(System.DateTime):
+                    System.DateTime dt = Convert.ToDateTime(_object);
+                return this.addObjectToListInternal<System.DateTime>(dt, List);
+                default:
+                    throw new Exception("Case unhandled exception. Method name:" + MethodBase.GetCurrentMethod().Name);
+            }
+        }
+        
+    }
+}
